dd5fc1ba156d2286be67a274cdb67b1abf4a21bf
Heehyun-Ryu <rhh221@naver.com>
2024-01-25 15:20:48 +0900

    "feat: server send to client event collecttion signal"

--- Websocket/client.py
+++ Websocket/client.py
@@ -1,8 +1,16 @@
 import websockets
 import asyncio
 import cv2
-import sys
 from websockets.exceptions import ConnectionClosedError
+
+async def receive_event(websocket):
+    try:
+        while True:
+            response = await websocket.recv()
+            print(f"{response}")
+
+    except websockets.exceptions.ConnectionClosedOK as e:
+        print("recive_event closed")
 
 async def send_video():
     capture = cv2.VideoCapture(0)
@@ -40,5 +48,31 @@ async def send_video():
 
         capture.release()
 
-asyncio.get_event_loop().run_until_complete(send_video())
+async def send_data(websocket):
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+    try:
+        while True:
+            ret, frame = capture.read()
+
+            _, img_encode = cv2.imencode('.jpg', frame)
+
+            await websocket.send(img_encode.tobytes())
+
+    except websockets.exceptions.ConnectionClosedOK as e:
+        print("ConnectionClosedOK - Server")
+
+async def main():
+    async with websockets.connect("ws://192.168.0.44:8000", ping_interval=None) as websocket:
+        print("connect!!")
+        await websocket.send('room1')
+
+        task_send = asyncio.create_task(send_data(websocket))
+        task_receive = asyncio.create_task(receive_event(websocket))
+
+        await asyncio.gather(task_send, task_receive)
+
+asyncio.run(main())
+# asyncio.get_event_loop().run_until_complete(send_video())
--- Websocket/server.py
+++ Websocket/server.py
@@ -25,16 +25,97 @@ def memory_usage():
     print(f"memory usage: {rss: 10.5f} MB")
     # [{message}]
 
+# async def receive_video(websockets):
+#     file = open('info.json', 'r')
+#
+#     print("connected!")
+#     print("websockets: ", websockets)
+#     print(websockets.remote_address)
+#
+#     client_id = id(websockets)
+#     clients[client_id] = websockets
+#
+#     print("pid:", os.getpid())
+#
+#     place = await websockets.recv()
+#
+#     print("client connected:", client_id)
+#     print("place:", place)
+#
+#     js = json.load(file)
+#     file.seek(0)
+#
+#     video_time = int(js['video_time'])
+#
+#     create_directory(place)
+#
+#     start_time = datetime.datetime.now()
+#     filename = start_time.strftime('%Y-%m-%d %H_%M_%S')
+#
+#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#     video = cv2.VideoWriter(f"{place}/{place}_{filename}.avi", fourcc, 30, (640, 480))
+#
+#     event_collect = False
+#
+#     try:
+#         while True:
+#             now = datetime.datetime.now()
+#             nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+#             nowDatetime_path = now.strftime('%Y-%m-%d %H_%M_%S')
+#
+#             data = await websockets.recv()
+#
+#             try:
+#                 js = json.load(file)
+#                 file.seek(0)
+#
+#             except json.decoder.JSONDecodeError:
+#                 print("JSON Error")
+#                 js = {}
+#
+#             if data == "None":
+#                 print("None data")
+#
+#             else:
+#                 if js.get('event') == '1' and not event_collect:
+#                     await websockets.send("event!!!")
+#                     event_collect = True
+#
+#                 frame = np.frombuffer(data, dtype=np.uint8)
+#                 frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+#
+#                 frame = Image.fromarray(frame)
+#                 draw = ImageDraw.Draw(frame)
+#                 draw.text(xy=(10, 5), text=nowDatetime, font=font, fill=(0, 0, 0))
+#                 frame = np.array(frame)
+#
+#                 # record video
+#                 current_time = datetime.datetime.now()
+#                 # if (current_time - start_time).total_seconds() >= video_time * 60:
+#                 if (current_time - start_time).total_seconds() >= 10:
+#                     video.release()
+#                     start_time = datetime.datetime.now()
+#                     filename = start_time.strftime('%Y-%m-%d %H_%M_%S')
+#                     video = cv2.VideoWriter(f"{place}/{place}_{filename}.avi", fourcc, 30, (640, 480))
+#
+#                 video.write(frame)
+#
+#                 cv2.imshow(str(client_id) + str(place), frame)
+#
+#                 cv2.waitKey(1)
+#
+#                 if cv2.getWindowProperty(str(client_id) + str(place), cv2.WND_PROP_VISIBLE) < 1:
+#                     video.release()
+#                     break
+#
+#     except ConnectionClosedOK as e:
+#         print("ConnectionClosedOK - Client shutting down?\n")
+#         cv2.destroyWindow(str(client_id) + str(place))
+#         print(e)
+#         del clients[client_id]
+
+
 async def receive_video(websockets):
-    async def control_record():
-        nonlocal record, start
-        record = False
-        video.release()
-        start = True
-
-    start = True
-    record = False
-
     file = open('info.json', 'r')
 
     print("connected!")
@@ -54,67 +135,67 @@ async def receive_video(websockets):
     js = json.load(file)
     file.seek(0)
 
-    video_time = js['video_time']
+    video_time = int(js['video_time'])
 
     create_directory(place)
 
     start_time = datetime.datetime.now()
     filename = start_time.strftime('%Y-%m-%d %H_%M_%S')
 
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video = cv2.VideoWriter(f"{place}/{place}_{filename}.avi", fourcc, 30, (640, 480))
+
+
+    event_collect = False
 
     try:
-        while True:/
+        while True:
             now = datetime.datetime.now()
             nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
             nowDatetime_path = now.strftime('%Y-%m-%d %H_%M_%S')
 
-            data = await websockets.recv()
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            video = cv2.VideoWriter(f"{place}/{place}_{nowDatetime_path}.avi", fourcc, 30, (640, 480))
+            print("start!!!")
 
-            try:
-                js = json.load(file)
-                file.seek(0)
+            for _ in range(1800):
+                data = await websockets.recv()
 
-            except json.decoder.JSONDecodeError:
-                print("Tlqkf")
-                js = json.load(file)
-                file.seek(0)
+                try:
+                    js = json.load(file)
+                    file.seek(0)
 
-            if data == "None":
-                print("Fuck")
+                except json.decoder.JSONDecodeError:
+                    print("JSON Error")
+                    js = {}
 
-            else:
-                if js['event'] == '1':
-                    print("I will receive event")
+                if data == "None":
+                    print("None data")
 
-                frame = np.frombuffer(data, dtype=np.uint8)
-                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                else:
+                    if js.get('event') == '1':
+                        await websockets.send("event!!!")
 
-                frame = Image.fromarray(frame)
-                draw = ImageDraw.Draw(frame)
-                draw.text(xy=(10, 5), text=nowDatetime, font=font, fill=(0, 0, 0))
-                frame = np.array(frame)
+                    frame = np.frombuffer(data, dtype=np.uint8)
+                    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
+                    now = datetime.datetime.now()
+                    nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
 
-                current_time = datetime.datetime.now()
-                if (current_time - start_time).total_seconds() >= int(video_time) * 60:
-                    video.release()
-                    start_time = datetime.datetime.now()
-                    filename = start_time.strftime('%Y-%m-%d %H_%M_%S')
-                    video = cv2.VideoWriter(f"{place}/{place}_{filename}.avi", fourcc, 30, (640, 480))
+                    frame = Image.fromarray(frame)
+                    draw = ImageDraw.Draw(frame)
+                    draw.text(xy=(10, 5), text=nowDatetime, font=font, fill=(0, 0, 0))
+                    frame = np.array(frame)
 
-                video.write(frame)
+                    video.write(frame)
 
-                cv2.imshow(str(client_id) + str(place), frame)
+                    cv2.imshow(str(client_id) + str(place), frame)
 
-                cv2.waitKey(1)
+                    cv2.waitKey(1)
 
-                if cv2.getWindowProperty(str(client_id) + str(place), cv2.WND_PROP_VISIBLE) < 1:
-                    video.release()
-                    break
-
-                # schedule.run_pending()
+                    if cv2.getWindowProperty(str(client_id) + str(place), cv2.WND_PROP_VISIBLE) < 1:
+                        video.release()
+                        break
+            video.release()
+            print("end!!!")
 
     except ConnectionClosedOK as e:
         print("ConnectionClosedOK - Client shutting down?\n")
